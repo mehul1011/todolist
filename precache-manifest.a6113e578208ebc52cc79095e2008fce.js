4ebb080a897364ff630f85798f2c76f36c064adf
mehul1011 <42713198+mehul1011@users.noreply.github.com>
2019-08-28 22:25:58 +0530

    Updates

---
+++ precache-manifest.a6113e578208ebc52cc79095e2008fce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f6a1d019855393ce5966fe7d657dbe5",
+    "url": "/robofriends-app/index.html"
+  },
+  {
+    "revision": "e3dcfacf558d2803e4cc",
+    "url": "/robofriends-app/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "5694e1898ca4c92fd14d",
+    "url": "/robofriends-app/static/js/2.6408f43b.chunk.js"
+  },
+  {
+    "revision": "e3dcfacf558d2803e4cc",
+    "url": "/robofriends-app/static/js/main.8cdb25bd.chunk.js"
+  },
+  {
+    "revision": "e44d24ad0a2645503a25",
+    "url": "/robofriends-app/static/js/runtime~main.12706f03.js"
+  }
+]);
